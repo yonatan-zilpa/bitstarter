bf4bb9660c79feb9caa75c6cbc20fe8ca3516522
yonatan-zilpa <idea2practice@gmail.com>
2013-07-23 13:13:18 +0000

    hello world index.html

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
-#!/usr/bin/env node
-var express = require('express');
-var app = express.createServer(express.logger());
-var fs = require('fs');
 
-app.get('/',function(request,response){
- response.send('Hello Wrold2 !');
-//response.send(fs.read.FileSync("./index.html").toString());
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+var fs = require('fs');
+var homepage = fs.readFileSync("index.html", "utf-8");
+response.send(homepage.toString('utf8'));
 });
 
-var port = process.env.PORT || 5000;
-   app.listen(port, function() {
-     console.log("Listening on " + port);
-   });
-
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
