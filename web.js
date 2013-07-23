3824b777d8c9c78824b9e0a6691d91a9c5beee7d
yonatan-zilpa <idea2practice@gmail.com>
2013-07-23 02:56:38 +0000

    with index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('index.html');
+  response.send("index.html");
 });
 
 var port = process.env.PORT || 5000;
