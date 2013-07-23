d0a8938b09d1885f81fe151edf81ce146711336a
yonatan-zilpa <idea2practice@gmail.com>
2013-07-23 12:35:12 +0000

    html

--- web.js
+++ web.js
@@ -4,25 +4,8 @@ app.use(express.logger());
 
 
 app.get('/', function(request, response) {
-fs = require('fs')
-fs.readFile('./index.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
+ response.send("hello world");
 });
-
-
-});
-/* response.send("hello world");
-var fs=require('./index.html');
-
-  fs.redFile('index.html', function (err, data) {
-	  if (err) throw err;
-	  console.log(data);
-	});
-*/
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
