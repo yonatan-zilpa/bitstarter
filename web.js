7d5549f2831f361e548d31dc45a269392f054e0f
yonatan-zilpa <idea2practice@gmail.com>
2013-07-23 13:05:02 +0000

    hello world index.html

--- web.js
+++ web.js
@@ -8,3 +8,8 @@ app.get('/',function(request,response){
 response.send(fs.read.FileSync("./index.html").toString());
 });
 
+var port = process.env.PORT || 5000;
+   app.listen(port, function() {
+     console.log("Listening on " + port);
+   });
+
