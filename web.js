4efcb70de53fb5cf2397c82a71da78d080d999ea
yonatan-zilpa <idea2practice@gmail.com>
2013-07-23 13:03:05 +0000

    hello world index.html

--- web.js
+++ web.js
@@ -1,14 +1,10 @@
+#!/usr/bin/env node
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var app = express.createServer(express.logger());
+var fs = require('fs');
 
-
-app.get('/', function(request, response) {
-// response.send("hello world");
-	response.send(fs.readFileSync("./index.html").toString()) ;
+app.get('/',function(request,response){
+// response.send('Hello Wrold2 !');
+response.send(fs.read.FileSync("./index.html").toString());
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
