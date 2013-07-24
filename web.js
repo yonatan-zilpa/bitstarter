75017ecfc546247cefa846dc51e67c798e01e66a
Ubuntu <ubuntu@ip-172-31-40-77.us-west-2.compute.internal>
2013-07-24 08:14:22 +0000

    couresera hw4

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
-
 var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
 var fs = require('fs');
-var homepage = fs.readFileSync("index.html", "utf-8");
-response.send(homepage.toString('utf8'));
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger())
+
+app.get('/', function(request, reesponse) {
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
